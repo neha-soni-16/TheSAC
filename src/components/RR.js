51f29fd73d65ab0ef39d2cd0105a74e6716ba238
neha-soni-16 <nehaachi16@gmail.com>
2021-11-18 21:36:08 +0530

    chart

--- src/components/RR.js
+++ src/components/RR.js
@@ -2,63 +2,47 @@ import React from "react";
 import Table from "./Table";
 
 const RR = (props) => {
-
-
     const findWaitingTime = (processData, n, wt, quantum) => {
-
         let rem_bt = new Array(n).fill(0);
         for (let i = 0; i < n; i++)
             rem_bt[i] = parseInt(processData[i].BurstTime);
- 
-        let t = 0; 
- 
+
+        let t = 0;
 
         while (1) {
             let done = true;
- 
+
             for (let i = 0; i < n; i++) {
-
                 if (rem_bt[i] > 0) {
-                    done = false; 
- 
+                    done = false;
+
                     if (rem_bt[i] > quantum) {
-
                         t += quantum;
- 
-
                         rem_bt[i] -= quantum;
-                    }
- 
-
-                    else {
-
+                    } else {
                         t = t + rem_bt[i];
-
                         wt[i] = t - parseInt(processData[i].BurstTime);
-
                         rem_bt[i] = 0;
                     }
                 }
             }
- 
-            if (done === true)
-                break;
+
+            if (done === true) break;
         }
-    }
+    };
 
     const findTurnAroundTime = (processData, n, wt, tat) => {
-
         for (let i = 0; i < n; i++)
             tat[i] = parseInt(processData[i].BurstTime) + wt[i];
-    }
+    };
 
-
-    const findavgTime = (processData,quantum) => {
-
+    const findavgTime = (processData, quantum) => {
         let n = processData.length;
 
-        let wt = new Array(n).fill(0), tat = new Array(n).fill(0);
-        let total_wt = 0, total_tat = 0;
+        let wt = new Array(n).fill(0),
+            tat = new Array(n).fill(0);
+        let total_wt = 0,
+            total_tat = 0;
 
         findWaitingTime(processData, n, wt, quantum);
 
@@ -67,23 +51,18 @@ const RR = (props) => {
         for (let i = 0; i < n; i++) {
             total_wt = total_wt + wt[i];
             total_tat = total_tat + tat[i];
- 
-          
         }
 
         console.log("Average waiting time = ", total_wt / n);
         console.log("Average turn around time = ", total_tat / n);
+    };
 
-    }
-
-
-    const handleCalculate = (event) =>{
-        
+    const handleCalculate = (event) => {
         event.preventDefault();
         let quantum = parseInt(event.target.timeQuantum.value);
 
-        findavgTime(props.processData,quantum);
-    }
+        findavgTime(props.processData, quantum);
+    };
 
     return (
         <div className="rr">
@@ -93,10 +72,9 @@ const RR = (props) => {
             />
 
             <form onSubmit={handleCalculate}>
-                <input type="number" name="timeQuantum"/>
+                <input type="number" name="timeQuantum" />
                 <button>Calculate</button>
             </form>
-
         </div>
     );
 };
